b1c2de4def59fd470ac70a38d93b3aa21accc854
Yogesh Parwani <yogeshparwani99.yp@gmail.com>
2024-08-28 14:09:20 +0530

    chore: initial commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('Action!'),
+        ),
+      ),
+    );
+  }
+}
