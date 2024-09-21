b05dbc10b389741c740845313de27ec83bf25822
Yogesh Parwani <yogeshparwani99.yp@gmail.com>
2024-09-21 18:54:42 +0530

    feat: ensure selected item is always in the viewport

--- lib/src/module/tasks/presentation/sections/action_view_selector.dart
+++ lib/src/module/tasks/presentation/sections/action_view_selector.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:figma_squircle/figma_squircle.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -8,11 +10,61 @@ import '../modals/add_action_modal.dart';
 import '../state/action_view_list_controller.dart';
 import '../state/selected_action_view_controller.dart';
 
-class ActionViewSelector extends ConsumerWidget with ActionViewModal {
+class ActionViewSelector extends ConsumerStatefulWidget {
   const ActionViewSelector({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<ConsumerStatefulWidget> createState() => _ActionViewSelectorState();
+}
+
+class _ActionViewSelectorState extends ConsumerState<ActionViewSelector> with ActionViewModal {
+  late final _scrollController = ScrollController();
+  final _selectorItemKeys = <GlobalKey>[];
+
+  @override
+  void initState() {
+    super.initState();
+
+    _initializeItemKeys();
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      ref.listenManual(selectedActionViewIndexController, (oldIndex, index) async {
+        _scrollToSelectedItem();
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  void _scrollToSelectedItem() {
+    final selectedIndex = ref.read(selectedActionViewIndexController);
+
+    if (selectedIndex < 0 || selectedIndex >= _selectorItemKeys.length) return;
+
+    final _itemContext = _selectorItemKeys[selectedIndex].currentContext;
+    if (_itemContext == null) return;
+
+    unawaited(Scrollable.ensureVisible(
+      _itemContext,
+      duration: const Duration(milliseconds: 300),
+      curve: Curves.easeInOut,
+      alignment: 0.7,
+    ));
+  }
+
+  void _initializeItemKeys() {
+    final actionViews = ref.read(actionViewList);
+    _selectorItemKeys
+      ..clear()
+      ..addAll(List.generate(actionViews.length, (_) => GlobalKey()));
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final actionViews = ref.watch(actionViewList);
 
     return Row(
@@ -34,6 +86,7 @@ class ActionViewSelector extends ConsumerWidget with ActionViewModal {
               radius: SmoothBorderRadius(cornerRadius: 12, cornerSmoothing: 1),
               child: SingleChildScrollView(
                 scrollDirection: Axis.horizontal,
+                controller: _scrollController,
                 child: Row(
                   children: List.generate(
                     actionViews.length,
@@ -42,6 +95,7 @@ class ActionViewSelector extends ConsumerWidget with ActionViewModal {
                       return _ActionSelectorItem(
                         actionView: actionView,
                         index: index,
+                        key: _selectorItemKeys[index],
                       );
                     },
                   ),
@@ -62,6 +116,7 @@ class _ActionSelectorItem extends ConsumerWidget {
   const _ActionSelectorItem({
     required this.actionView,
     required this.index,
+    super.key,
   });
 
   final TaskView actionView;
